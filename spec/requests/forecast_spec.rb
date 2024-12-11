286e6695d61a08415569b8771da0eb90c27f3b0b
Lisa Sheridan <lisa.sheridan@gmail.com>
2024-12-10 14:47:21 -1000

    Add high, low and tests

--- spec/requests/forecast_spec.rb
+++ spec/requests/forecast_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Forecast", type: :request do
-  RSpec.shared_examples "a forecast search prompt" do |options|
+  RSpec.shared_examples "a forecast search prompt" do |options = {}|
     it 'renders the show template' do
       make_request
       follow_redirect! if options[:follow_redirect]
@@ -15,43 +15,66 @@ RSpec.describe "Forecast", type: :request do
     end
   end
 
+  RSpec.shared_examples "a response without forecast data" do |options = {}|
+    it 'does not render the forecast data' do
+      make_request
+      follow_redirect! if options[:follow_redirect]
+      assert_select("[data-testid='forecast-data']", false)
+    end
+  end
+
+  RSpec.shared_examples "a response with forecast data" do |options = {}|
+    it 'renders the forecast data' do
+      make_request
+      follow_redirect! if options[:follow_redirect]
+      assert_select("[data-testid='forecast-data']")
+    end
+
+    it 'renders the current temp' do
+      make_request
+      follow_redirect! if options[:follow_redirect]
+      assert_select("[data-testid='current-temp']", /.+/)
+    end
+
+    it 'renders the max temp' do
+      make_request
+      follow_redirect! if options[:follow_redirect]
+      assert_select("[data-testid='max-temp']", /.+/)
+    end
+
+    it 'renders the min temp' do
+      make_request
+      follow_redirect! if options[:follow_redirect]
+      assert_select("[data-testid='min-temp']", /.+/)
+    end
+
+    it 'does not render errors' do
+      make_request
+      follow_redirect! if options[:follow_redirect]
+      assert_select("[data-testid='errors']", false)
+    end
+  end
+
   describe "GET /" do
     subject(:make_request) { get '/' }
 
-    it_behaves_like "a forecast search prompt", {}
-
-    it 'does not render the current temp' do
-      make_request
-      assert_select("[data-testid='current-temp']", false)
-    end
+    it_behaves_like "a forecast search prompt"
+    it_behaves_like "a response without forecast data"
   end
 
   describe "GET /show" do
     subject(:make_request) { get '/forecast' }
 
-    it_behaves_like "a forecast search prompt", {}
-
-    it 'does not render the current temp' do
-      make_request
-      assert_select("[data-testid='current-temp']", false)
-    end
+    it_behaves_like "a forecast search prompt"
+    it_behaves_like "a response without forecast data"
   end
 
   describe "GET /show/:zip", vcr: { cassette_name: "forecast/show_zip" }  do
     subject(:make_request) { get "/forecast/?zip=#{zip}&commit=Search" }
     let(:zip) { "96754" }
 
-    it_behaves_like "a forecast search prompt", {}
-
-    it 'renders the current temp' do
-      make_request
-      assert_select("[data-testid='current-temp']", /.+/)
-    end
-
-    it 'does not render errors' do
-      make_request
-      assert_select("[data-testid='errors']", false)
-    end
+    it_behaves_like "a forecast search prompt"
+    it_behaves_like "a response with forecast data"
   end
 
   describe "GET /show/:zip invalid", vcr: { cassette_name: "forecast/show_zip_invalid" }  do
@@ -59,13 +82,7 @@ RSpec.describe "Forecast", type: :request do
     let(:zip) { "invalid" }
 
     it_behaves_like "a forecast search prompt", follow_redirect: true
-
-    it 'does not render the current temp' do
-      make_request
-      expect(response).to redirect_to("/forecast")
-      follow_redirect!
-      assert_select("[data-testid='current-temp']", false)
-    end
+    it_behaves_like "a response without forecast data", follow_redirect: true
 
     it 'renders the errors' do
       make_request
